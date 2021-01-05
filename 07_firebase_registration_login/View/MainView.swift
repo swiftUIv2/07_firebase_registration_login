9029af4ca754638172f02369bf74d08b09ade8f2
emm <723125@gmail.com>
2021-01-05 15:58:04 +0000

    firebase sdk installed, set-up file

---
+++ 07_firebase_registration_login/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  07_firebase_registration_login
+//
+//  Created by emm on 05/01/2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
