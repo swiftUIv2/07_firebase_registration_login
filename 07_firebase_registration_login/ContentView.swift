35fd608e2f467cd0a7ba1241fa1c6b30a3c74964
emm <723125@gmail.com>
2021-01-05 13:08:49 +0000

    Initial Commit

---
+++ 07_firebase_registration_login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  07_firebase_registration_login
+//
+//  Created by emm on 05/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 07_firebase_registration_login/_7_firebase_registration_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_firebase_registration_loginApp.swift
+//  07_firebase_registration_login
+//
+//  Created by emm on 05/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct _7_firebase_registration_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
